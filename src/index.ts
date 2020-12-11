8fc829c4ce559c952826c81ed307e75aeb1b686f
taoqf <tao_qiufeng@126.com>
2020-12-11 17:17:44 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
 
-export default function an38(param1: string) {
+export default function an38<T>(data: T[]) {
+	return data.map((it) => {
+		const keys = Object.keys(it) as (keyof T)[];
+		return keys.reduce((pre, key) => {
+			const value = it[key];
+			if (typeof value === 'bigint') {
+				return {
+					...pre,
+					[key]: Number(value)
+				};
+			}
+			return {
+				...pre,
+				[key]: value
+			};
+		}, {} as T);
+	});
 }
--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,12 @@ const test = require('ava');
 
 const { default: a } = require('../dist/index');
 
-test('xxx', (t) => {
-	t.pass();
+test('bigint类型转换=>正常转换', (t) => {
+	const r = a([{ a: 1n }, { b: 2n }, { c: 3n }]);
+	t.deepEqual(r, [{ a: 1 }, { b: 2 }, { c: 3 }])
+});
+
+test('bigint类型转换=>丢失精度', (t) => {
+	const r = a([{ big: 111111111111111111n }]);
+	t.deepEqual(r, [{ big: 111111111111111100 }])
 });
